a34d0081009ecc4db3aa1591cf215ff6dc02f4e2
prostotema1 <artemkorolev-2002@yandex.ru>
2022-12-01 09:07:53 +0300

    Исправление ошибок

--- back-end/src/java/com/app/SpringBootStarter.java
+++ back-end/src/java/com/app/SpringBootStarter.java
@@ -3,10 +3,11 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringBootStarter {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootStarter.class);
+        SpringApplication.run(SpringBootStarter.class,args);
     }
 }
