1b73e76c609bd9e56d66619177e19a2b8206d2c7
Evgeniy <st087790@student.spbu.ru>
2022-12-13 02:46:03 +0300

    added mainPageController.java

--- back-end/src/main/java/com/app/SpringBootStarter.java
+++ back-end/src/main/java/com/app/SpringBootStarter.java
@@ -3,8 +3,12 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaRepositories("com.app.repositories")
 public class SpringBootStarter {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class,args);
--- back-end/src/main/java/com/app/repositories/CourseWorkRepository.java
+++ back-end/src/main/java/com/app/repositories/CourseWorkRepository.java
@@ -1,6 +1,8 @@
 package com.app.repositories;
 
 import com.app.db.entities.CourseWork;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ public interface CourseWorkRepository extends JpaRepository<CourseWork,Integer> 
     CourseWork findByCourseWorkId(String id);
 
     Boolean existsByTitle(String title);
+
+    List<CourseWork> findByIdIn(Collection<Integer> ids);
 }
