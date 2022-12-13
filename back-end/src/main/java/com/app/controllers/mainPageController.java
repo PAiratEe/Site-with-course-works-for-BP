8f4fc47c5b2eb332b1c36bedc440edcc13b3a666
prostotema1 <artemkorolev-2002@yandex.ru>
2022-12-13 13:58:34 +0300

    Добавлен вывод

---
+++ back-end/src/main/java/com/app/controllers/mainPageController.java
@@ -0,0 +1,28 @@
+package com.app.controllers;
+
+
+import com.app.repositories.CourseWorkRepository;
+import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mainPageController {
+    @Autowired
+    private CourseWorkRepository courseWorkRepository;
+
+    @GetMapping("/main")
+    private ResponseEntity<?> displayPage(@RequestParam @PathVariable int page) {
+        var firstId = 20 * (page - 1) + 1;
+        var courseWorks = courseWorkRepository.findByCourseWorkIdIn(IntStream.range(firstId, firstId + 20).boxed().toList());
+        if(courseWorks == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(courseWorks, HttpStatus.OK);
+    }
+}
