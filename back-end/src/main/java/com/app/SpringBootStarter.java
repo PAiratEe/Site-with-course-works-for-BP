103ea8f9dc349a4665bf955969f50210745e2ad0
prostotema1 <artemkorolev-2002@yandex.ru>
2022-12-13 13:54:39 +0300

    Revert "added mainPageController.java"
    This reverts commit 1b73e76c609bd9e56d66619177e19a2b8206d2c7.

--- back-end/src/main/java/com/app/SpringBootStarter.java
+++ back-end/src/main/java/com/app/SpringBootStarter.java
@@ -3,12 +3,8 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableJpaRepositories("com.app.repositories")
+@SpringBootApplication
 public class SpringBootStarter {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class,args);
--- back-end/src/main/java/com/app/repositories/CourseWorkRepository.java
+++ back-end/src/main/java/com/app/repositories/CourseWorkRepository.java
@@ -1,8 +1,6 @@
 package com.app.repositories;
 
 import com.app.db.entities.CourseWork;
-import java.util.Collection;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +10,4 @@ public interface CourseWorkRepository extends JpaRepository<CourseWork,Integer> 
     CourseWork findByCourseWorkId(String id);
 
     Boolean existsByTitle(String title);
-
-    List<CourseWork> findByIdIn(Collection<Integer> ids);
 }
