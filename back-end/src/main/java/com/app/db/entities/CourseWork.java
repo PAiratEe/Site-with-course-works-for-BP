5f01727447c58a786c4c6e08cb2ba49e6cc4c6de
prostotema1 <artemkorolev-2002@yandex.ru>
2022-12-13 13:55:40 +0300

    Revert "Merge pull request #17 from prostotema1/back-end"
    This reverts commit 48549b66ef854364814cefb97b67baf6d4be9055, reversing
    changes made to ab83a050ceb295de9b65bfc95c5cf3660184685c.

--- back-end/src/main/java/com/app/db/entities/CourseWork.java
+++ back-end/src/main/java/com/app/db/entities/CourseWork.java
@@ -31,9 +31,6 @@ public class CourseWork {
     @JoinColumn(name = "professorid", referencedColumnName = "professorId")
     private Professor professor;
 
-    @ManyToOne(cascade = CascadeType.ALL, optional = false)
-    private Tag_CourseWork courseworkid;
-
     public Long getCourseWorkId() {
         return this.courseWorkId;
     }
@@ -82,8 +79,6 @@ public class CourseWork {
         return this.Description;
     }
 
-    public Tag_CourseWork getCourseworkid() { return courseworkid; }
-
     public void setShortDescription(String ShortDescription) {
         this.ShortDescription = ShortDescription;
     }
@@ -91,6 +86,4 @@ public class CourseWork {
     public void setDescription(String Description) {
         this.Description = Description;
     }
-
-    public void setCourseworkid(Tag_CourseWork courseworkid) { this.courseworkid = courseworkid; }
 }
